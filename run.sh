149cd503b3e1d0207fdaf96579dfad19663a4269
Joel Brennecke <joel.brennecke@gmail.com>
2025-11-16 20:37:01 -0500

    Fix run.sh to initialize conda properly

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+
+# Initialize conda for non-interactive shells
+source /home/joelb/miniconda3/etc/profile.d/conda.sh
+
 conda activate ai_env
 uvicorn app.main:app --reload --host 0.0.0.0 --port 8000
+
